bc9a667782db80874147763a365610e5f9033993
Ignacio <dysrupterwow@gmail.com>
2020-11-22 17:14:09 -0300

    Terrible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib._cm_listed import cmaps
+
 
 def armar_paises(datos) : # Aca armo una listita de paises para despues tener donde comparar en la otra funcion
     lista = []
@@ -14,22 +16,23 @@ def armar_casos(datos,lista_paises):
     for localizacion in lista_paises: #Lista paises es una lista de cada localizacion
         for key , value in datos.items(): #Buscamos que por cada pais me arme los casos y las fechas
 
-            #if (localizacion == value["location"]):
-                #casos.append(value["new_cases"])
-                #fecha.append(value["date"])
-        #armar_plot(casos,fecha,value["location"])#Aca mandamos la lista con los casos, y la fecha para hacer los plots, y el nombre unico del pais que no deberia variar
+            if (localizacion == value["location"] and value["new_cases"] != 0):
+                casos.append(value["new_cases"])
+                fecha.append(value["date"])
+        armar_plot(casos,fecha,value["location"])#Aca mandamos la lista con los casos, y la fecha para hacer los plots, y el nombre unico del pais que no deberia variar
 
 
-#"def armar_plot(casos,fechas,localidad):
-    #plt.figure(figsize=(18,6))
-    #plt.xlabel('Fechas')
-    #plt.ylabel('Casos Diarios')
-    #plt.xticks(rotation=90)
-    #plt.plot(fechas,casos)
-    #plt.yscale('log')
-    #plt.xscale('log')
-    #plt.grid()
-   # plt.show()
+def armar_plot(casos,fechas,localidad):
+    plt.figure(figsize=(18,6))
+    plt.xlabel('Fechas')
+    plt.ylabel('Casos Diarios')
+    plt.scatter(fechas,casos)
+    plt.xticks(rotation=90)
+    plt.grid(True)
+    plt.plot(fechas,casos,label=localidad)
+    plt.show()
+    casos.clear()
+    fechas.clear()
 
 
 
@@ -39,7 +42,10 @@ archivo = pd.read_csv("full_data.csv",usecols=['date','location','new_cases']) #
 datos = archivo.to_dict("index") # Esto queda indexado como : {'date': '2020-01-01', 'location': 'Afghanistan', 'new_cases': 0.0}
 paises_total = []
 paises_total = armar_paises(datos)
-print(paises_total)
+#q pasises_a_buscar=input()
+#print("Que paises con covid desea graficar? ELiga de la siguiente lista : /n",paises_total)
+#paises_buscados = input()
+#print(paises_buscados)
 armar_casos(datos,paises_total)
 
 
