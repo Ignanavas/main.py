c031ca1884a76d62a38d16ce093a4c1b77aae2ba
Ignacio <dysrupterwow@gmail.com>
2020-11-23 21:45:24 -0300

    Grafico andando Funciona el menu, y se puede elegir que pais graficar, con control de error

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from datetime import datetime
 
 from matplotlib.legend_handler import HandlerLine2D
 
+def Convertir(string):
+    li = list(string.split(" "))
+    return li
 
 def armar_paises(datos):  # Aca armo una listita de paises para despues tener donde comparar en la otra funcion
     lista = []
@@ -14,7 +17,7 @@ def armar_paises(datos):  # Aca armo una listita de paises para despues tener do
     return lista
 
 
-def armar_casos(datos, lista_paises):
+def armar_casos(datos,lista_paises):
     casos = []
     fecha = []
     muertes = []
@@ -48,14 +51,26 @@ def armar_plot(casos_totales, fechas, localidad, muertes_totales):
     muertes_totales.clear()
 
 
-archivo = pd.read_csv("full_data.csv", usecols=['date', 'location', 'total_cases',
-                                                'total_deaths'])  # Bastante util el usecols, me deja elegir que columnas del cvs quiero usar, en vez de tener que complicarme con un drop del dataframe.
+archivo = pd.read_csv("full_data.csv", usecols=['date', 'location', 'total_cases','total_deaths'])
+# Bastante util el usecols, me deja elegir que columnas del cvs quiero usar, en vez de tener que complicarme con un drop del dataframe.
 
-datos = archivo.to_dict("index")  # Esto queda indexado como : {'date': '2020-01-01', 'location': 'Afghanistan', 'new_cases': 0.0}
+datos = archivo.to_dict("index")
 paises_total = []
 paises_total = armar_paises(datos)
-print("Que paises con covid desea graficar? ELiga de la siguiente lista :\n",paises_total)
-lista_paises = input()
-# paises_buscados = input()
-# print(paises_buscados)dsd
-armar_casos(datos, paises_total)
+lista_paises = []
+centinela = 'Chau'
+while centinela != 'Hola': # un control de error humilde, que no este vacia la lista, y que exista en la lista de datos que tenemos
+    print("Que paises con covid desea graficar? ELiga de la siguiente lista :\n",paises_total)
+    lista_paises = input()
+
+
+    if not lista_paises:
+        print("Error! La lista esta vacia!")
+    elif(not lista_paises in paises_total):
+        print("Error! Ese pais no se encuentra en el csv de paises! Intente con otro!")
+    else:
+        centinela = 'Hola'
+        lista_paises = Convertir(lista_paises)
+else:
+
+    armar_casos(datos,lista_paises)
